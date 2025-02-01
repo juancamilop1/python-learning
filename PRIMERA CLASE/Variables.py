f2f14910ce2ab025a9c9a1f0b1db290902f9feed
juancamilop1 <159964422+juancamilop1@users.noreply.github.com>
2025-02-01 17:39:11 -0500

    CLASE 1
    en esta clase aprendimos a diferenciar lo que es un interprete de un compilador y tambien que son la variables

---
+++ PRIMERA CLASE/Variables.py
@@ -0,0 +1,39 @@
+"""VARIABLES
+es un espacio de la memoria donde se guardan y se recuperan datos que utliza un programa
+Cada una debe tener un nombre en el cual pueda identificarse y referirse a ella.
+El nombre de una variable no puede coincidir con los nombres de los comandos asignados, este no debera contener espaciosen blanco, es decir que no se podria llamar print o nom bre 
+Los mas comunes son los que almacenan nombre texto (strings(str)) este siempre se encuentra entre comillas  o numeros (int o float)
+
+Python identifica el tipo de variable que se esta almacenando 
+
+DECLARACION DE VARIABLES 
+
+numero_uno = 2
+numero_uno = 2.5
+numero_uno = "hola mundo"
+
+ESTE PUEDE DIFERENCIAR LAS MAYUSCULAS
+
+numero_uno = 2
+Numero_uno = 2.5
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+"""
+print ("Esto es una suma")
+
+numero_uno=2
+
+numero_dos=4
+
+resultado=numero_uno+numero_dos
+
+print("SU RESULTADO ES: ",resultado)
+
+#esto es un ejemplo para diferenciar mayusculas
+
+print ("Esto es una resta con ejemplo de diferencias de mayusculas")
+
+Numero_uno = 2
+numero_Uno = 2.5
+NUMERO_uno = "Hola mundo"
+resultado=Numero_uno-numero_Uno
+print("Este es un texto",NUMERO_uno," y esta es una resta",resultado )
---
+++ PRIMERA CLASE/diferencia enttre interprete y compilador .py
@@ -0,0 +1,21 @@
+"""diferencia enttre interprete y compilador 
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+    Interpretador Python: es aquel que realiza la traduccion de lenguaje de programacion a lenguaje maquina instruccion por instruccion
+       - Se ejecuta directamente sobre el código fuente.
+       - Se puede corregir y ejecutar línea por línea.
+       - Puede ser usado para prototipado y desarrollo rápido.
+       
+       EJEMPLO: 
+       print ("Hola mundo")
+       HOLA MUNDO
+       
+--------------------------------------------------------------------------------------------------------------------------------------------------------"""
+"""compilador de python: Un compilador es un programa que traduce código fuente de un lenguaje de programación a código máquina para que la computadora lo ejecute.
+    -Se compila el código fuente a bytecode.
+    -Este bytecode se ejecuta en un intérprete de Python.
+    -No se puede corregir y no se puede ejecutar línea por línea."""
+       
+print ("Hola mundo")
+print ("estoy aprendiendo python")
+
+
